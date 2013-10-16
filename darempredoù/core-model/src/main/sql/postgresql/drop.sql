8f759634bd8f03202f9a260646b298fded9e35f0
demis27@gmail.com <demis27@gmail.com@a2f809c3-3aa1-95dd-4ac8-9c850a417096>
2013-10-16 14:46:43 +0000

    Repository spring mode

---
+++ darempredoù/core-model/src/main/sql/postgresql/drop.sql
@@ -0,0 +1,13 @@
+drop table if exists "darempredoù".application_user cascade;
+drop table if exists "darempredoù".enterprise cascade;
+drop table if exists "darempredoù".enterprise_group cascade;
+drop table if exists "darempredoù".etablishment cascade;
+drop table if exists "darempredoù".user_group cascade;
+drop table if exists "darempredoù".user_group_label cascade;
+
+drop table if exists "darempredoù".ht_application_user cascade;
+drop table if exists "darempredoù".ht_enterprise cascade;
+drop table if exists "darempredoù".ht_enterprise_group cascade;
+drop table if exists "darempredoù".ht_etablishment cascade;
+drop table if exists "darempredoù".ht_user_group cascade;
+drop table if exists "darempredoù".ht_user_group_label cascade;
---
+++ darempredoù/darempredou-webapp/src/main/java/org/demis/darempredou/config/ApplicationContext.java
@@ -0,0 +1,57 @@
+package org.demis.darempredou.config;
+
+import org.hibernate.ejb.HibernatePersistence;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class ApplicationContext {
+
+    @Resource
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/darempredoù");
+        dataSource.setUsername("darempredoù");
+        dataSource.setPassword("darempredoù");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() throws ClassNotFoundException {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() throws ClassNotFoundException {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan("org.demis.darempredou.domain");
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+
+        Properties jpaProterties = new Properties();
+        jpaProterties.put("hibernate.dialect", "org.hibernate.dialect.ProgressDialect");
+
+        entityManagerFactoryBean.setJpaProperties(jpaProterties);
+
+        return entityManagerFactoryBean;
+    }
+}
