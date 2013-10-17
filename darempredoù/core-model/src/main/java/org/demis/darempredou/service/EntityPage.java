817f0225d1e9d7dd95c8d576277ccf81d583f0ec
demis27@gmail.com <demis27@gmail.com@a2f809c3-3aa1-95dd-4ac8-9c850a417096>
2013-10-17 14:57:09 +0000

    Paging

---
+++ darempredoù/core-model/src/main/java/org/demis/darempredou/service/EntityPage.java
@@ -0,0 +1,80 @@
+package org.demis.darempredou.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityPage<T> {
+
+    private List<T> elements = new ArrayList<T>();
+
+    private int size;
+
+    private int number;
+
+    private int totalElements;
+
+    private int totalPage;
+
+    private boolean hasNext;
+
+    private boolean hasPrevious;
+
+    public EntityPage() {
+    }
+
+    public List<T> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<T> elements) {
+        this.elements = elements;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(int totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPage() {
+        return totalPage;
+    }
+
+    public void setTotalPage(int totalPage) {
+        this.totalPage = totalPage;
+    }
+
+    public boolean isHasNext() {
+        return hasNext;
+    }
+
+    public void setHasNext(boolean hasNext) {
+        this.hasNext = hasNext;
+    }
+
+    public boolean isHasPrevious() {
+        return hasPrevious;
+    }
+
+    public void setHasPrevious(boolean hasPrevious) {
+        this.hasPrevious = hasPrevious;
+    }
+}
